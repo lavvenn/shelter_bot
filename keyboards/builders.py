e721d9d5e10a01195a3b0db9d66f75cfb0e765e8
lavvenn <zmievsijkonstantin0@gamil.com>
2024-04-20 07:04:16 +0300

    change card button text

--- keyboards/builders.py
+++ keyboards/builders.py
@@ -2,5 +2,5 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 def print_kards(cards: list)->InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
-    [builder.button(text=card, callback_data=f"open{card}") for card in cards]
+    [builder.button(text=f"<--{card}-->", callback_data=f"open{card}") for card in cards]
     return builder.as_markup()
